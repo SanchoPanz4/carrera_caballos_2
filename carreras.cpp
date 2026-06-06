8f9663b4cbfa61189d76e0719d15874a0e67fca6
edi <eramosg2003@gmail.com>
2026-06-05 23:34:06 -0400

    a

--- carreras.cpp
+++ carreras.cpp
@@ -211,8 +211,8 @@ while (caballo_que_corre.vueltas_realizadas <= vueltas_a_correr)//mientras no ha
 	    caballo_que_corre.metros_recorridos--;						  
 
 	    resetear_caballo(caballo_que_corre);//lo devuelve al inicio
-        if (sumatoria_vueltas > 7){
-            int metros_extra = sumatoria_vueltas - 7;
+        if (vueltas_a_correr > 1){
+            int metros_extra = sumatoria_vueltas - n_caballos;
             mvwprintw(pista,15, 1, "%4dm V:%2d |",sumatoria_metros- metros_extra,sumatoria_vueltas);//imprime actualizacion
         } else {
             mvwprintw(pista,15, 1, "%4dm V:%2d |",sumatoria_metros,sumatoria_vueltas);//imprime actualizacion
